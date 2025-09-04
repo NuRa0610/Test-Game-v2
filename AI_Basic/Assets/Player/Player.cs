3cbb8891883ff637418a66cfa390491dcb195ed4
Nu'man Zainul Rahman <71974543+NuRa0610@users.noreply.github.com>
2025-09-04 11:51:47 +0700

    Add player placeholder

--- AI_Basic/Assets/Player/Player.cs
+++ AI_Basic/Assets/Player/Player.cs
@@ -6,13 +6,34 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     //void Start() { }
+    private Rigidbody _rigidBody;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Transform _camera;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+        Vector3 horizontalDirection = horizontalInput * _camera.right;
+        Vector3 verticalDirection = verticalInput * _camera.forward;
+        verticalDirection.y = 0;
+        horizontalDirection.y = 0;
+        Vector3 movementDirection = horizontalDirection + verticalDirection;
+        _rigidBody.MovePosition(_rigidBody.position + movementDirection * _speed * Time.deltaTime);
         Debug.Log("Horizontal Input: " + horizontalInput);
         Debug.Log("Vertical Input: " + verticalInput);
     }
